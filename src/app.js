9405c3c5b308f8e12791496eb7a873d46ab2d126
Milben Tan <milben.tan@breakingwave.io>
2024-10-07 09:56:55 -0400

    deployment instruction

--- src/app.js
+++ src/app.js
@@ -1,7 +1,47 @@
 const express = require('express');
 const app = express();
+const { Web3 } = require("web3");
+const fs = require("fs");
+
 app.get("/msg", (req, res, next) => {
     res.json({ "message": "Hello, Spencer!" });
+});
+app.post('/registerBond', async(req,res)=>{
+
+});
+app.get("/deploy", async (req, res) => {
+    const { abi, bytecode } = JSON.parse(fs.readFileSync("DBToken.json"));
+    const network = process.env.ETHEREUM_NETWORK;
+    const INFURA_API_KEY = process.env.INFURA_PROJECT_ID;
+    console.log('kEY', network, INFURA_API_KEY)
+    const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+            `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+            { timeout: 10e3 }
+        ),
+    );
+    // Creating a signing account from a private key
+    const signer = web3.eth.accounts.privateKeyToAccount(
+        '0x' + process.env.SIGNER_PRIVATE_KEY,
+    );
+    web3.eth.accounts.wallet.add(signer);
+
+    // Using the signing account to deploy the contract
+    const contract = new web3.eth.Contract(abi);
+    contract.options.data = bytecode;
+    const deployTx = contract.deploy("DB TEST COIN", "DBCOIN", 50000);
+    const deployedContract = await deployTx
+        .send({
+            from: signer.address,
+            gas: await deployTx.estimateGas(),
+        })
+        .once("transactionHash", (txhash) => {
+            console.log(`Mining deployment transaction ...`);
+            console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+        });
+    // The contract is now deployed on chain!
+    console.log(`Contract deployed at ${deployedContract.options.address}`);
+    res.json({ "message": `Contract deployed at ${deployedContract.options.address}` });
 });
 app.listen(3000, () => {
     console.log("Server running on port 3000");
