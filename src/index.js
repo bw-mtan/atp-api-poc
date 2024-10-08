983ad2864050af0d0a94ed3017cede1338f09b42
Milben Tan <milben.tan@breakingwave.io>
2024-10-08 14:01:26 -0400

    new bond

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const cors = require('cors');
 
 // initialize an Express application
 const app = express();
 app.use(express.json());
+var corsOptions = {
+    origin: ['http://localhost:3000','*'],
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+app.use('/',cors(corsOptions));
 
 // connect to the "src/routers" directory
 const routersPath = path.join(__dirname, "routes");
-
+app.get("/", (req, res) => {
+    res.json("Welcome to ATP API");
+})
 // read all files in the "/src/routers" directory
 fs.readdirSync(routersPath).forEach((file) => {
-  if (file.endsWith(".js")) {
-    // dynamically import the router module
-    const routerModule = require(path.join(routersPath, file));
+    if (file.endsWith(".js")) {
+        // dynamically import the router module
+        const routerModule = require(path.join(routersPath, file));
 
-    // get the "router" object exported by the router module
-    const router = routerModule.router;
+        // get the "router" object exported by the router module
+        const router = routerModule.router;
 
-    // register the router
-    app.use(router);
-  }
+        // register the router
+        app.use(router);
+    }
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
