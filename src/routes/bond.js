16f79802240ce7d75176ea949d9374610707a47f
Milben Tan <milben.tan@breakingwave.io>
2024-10-16 10:41:14 -0400

    update

--- src/routes/bond.js
+++ src/routes/bond.js
@@ -4,4 +4,5 @@ const { BondController } = require("../controllers/bond.js");
 const router = new Router();
 router.post("/api/v1/bond/register", BondController.registerBond);
 router.post("/api/v1/bond/transfer", BondController.transferBond);
+router.get("/api/v1/bond/list", BondController.listAllBond);
 module.exports = { router };
